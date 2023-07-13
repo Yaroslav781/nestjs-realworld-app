fd461810f23fef43f531b574ee9a2b072436b61a
Yaroslav781 <pelexach12@gmail.com>
2023-07-13 11:10:42 +0300

    half of project

---
+++ src/article/article.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { User } from '@app/user/decorators/user.decorator';
+import { UserEntity } from '@app/user/user.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { ArticleResponseInterface } from './types/articleResponse.interface';
+
+@Controller('articles')
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(':slug')
+  async getSingleArticle(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.findBySlug(slug);
+    return this.articleService.buildArticleResponse(article);
+  }
+}
