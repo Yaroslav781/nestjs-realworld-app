510744a7f655bcd0e53f352ffb1474d1bde20026
Yaroslav781 <pelexach12@gmail.com>
2023-07-31 21:38:18 +0300

    fixed few mistakes

--- src/shared/pipes/backendValidation.pipe.ts
+++ src/shared/pipes/backendValidation.pipe.ts
@@ -6,13 +6,15 @@ import {
 } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { ValidationError, validate } from 'class-validator';
-import { error } from 'console';
 
 export class BackendValidationPipe implements PipeTransform {
   async transform(value: any, metadata: ArgumentMetadata) {
     const object = plainToClass(metadata.metatype, value);
+
+    if (typeof object !== 'object') {
+      return value;
+    }
     const errors = await validate(object);
-    console.log('transform', object, errors);
 
     if (errors.length === 0) {
       return value;
